99eb5027d3f23abc7bf7c743672c16c8e572f898
Akshat_23 <146257084+AkshatIITK@users.noreply.github.com>
2024-05-07 18:48:03 +0000

    modified:   src/components/CustomCard1.jsx 	modified:   src/components/footer.jsx 	modified:   src/output.css

--- src/components/CustomCard1.jsx
+++ src/components/CustomCard1.jsx
@@ -18,7 +18,7 @@ export default function CustomCard1({animation, image , text , heading}) {
   }, [])
   return (
     <div  data-aos={animation} data-aos-offset="200" data-aos-easing="ease-in-sine" data-aos-duration="600">
-    <MDBCard className='hover:scale-105 mt-4 dark:bg-black dark:text-white border-2'>
+    <MDBCard className='hover:scale-[1.02] mt-3 dark:bg-black dark:text-white border-2'>
       <MDBCardImage position='top' alt='...' src={image} />
       <MDBCardBody className='font-serif'>
         <MDBCardTitle className='text-2xl font-[500]'>{heading}</MDBCardTitle>
@@ -33,7 +33,7 @@ export default function CustomCard1({animation, image , text , heading}) {
       </MDBListGroup> */}
       <MDBCardBody className=''>
         {/* <MDBCardLink href='#'>Card link</MDBCardLink> */}
-        <MDBCardLink className="text-nav-links hover:text-white hover:bg-nav-links p-3 rounded-md" href='#'>ReadMore</MDBCardLink>
+        <MDBCardLink className="text-nav-links hover:text-white hover:bg-nav-links py-2 px-3 rounded-md font-[500]" href='#'>ReadMore</MDBCardLink>
       </MDBCardBody>
     </MDBCard>
     </div>
